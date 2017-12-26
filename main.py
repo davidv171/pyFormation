1bf11a5cfd69d9e4566fa9ee77b255eaa6ffb85e
davidv171 <david.vuckovic7@gmail.com>
2017-12-26 02:44:10 +0100

    Rebasing the whole thing because of OS issues.
    Added own probability

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import urllib.request as url
 from bs4 import BeautifulSoup
 import re
 import collections
+import operator
+
 
 def get_text():
     #A function that trims out the HTML tags
@@ -31,14 +33,35 @@ def split_text(input_text):
     del word_list[:n]
     return word_list
 
+
 def word_counter(input_text):
     #A method that counts how many times each word has been used and saves it in an array
     #Also makes it all lower case
     return collections.Counter(map(str.lower, input_text))
 
+
+def own_probability(input_text, number_of_words):
+    #A method that takes the input map/list and calculates the own probability
+    #Uses the formula: number of repetitions of a word divided by all words
+    all_values= list(input_text.values())
+    all_words = list(input_text.keys())
+    print(all_values)
+    print(all_words)
+    own_probs = [x / number_of_words for x in all_values]
+    print(own_probs)
+    new_dict = dict(zip(all_words, own_probs))
+    new_dict = sorted(new_dict.items(), key=operator.itemgetter(1))
+    return new_dict
+
+
 text = get_text()
-
 words = split_text(text)
-
+word_length = len(words)
 print(words)
-print(word_counter(words))
+word_count = word_counter(words)
+print("Word counter:")
+print(word_count)
+own_probabilities = own_probability(word_count, word_length)
+print("Own probabilities(ascending):")
+print(own_probabilities)
+
