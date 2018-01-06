570fc72a503cde7971aabad94acc289dc436ca10
davidv171 <david.vuckovic7@gmail.com>
2018-01-06 16:52:17 +0100

    Added a function that calculates entropy in case the probability of every value is the same. Added a function that calculates the entropy of every word in case we're using each word's own probability.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import re
 import collections
 import operator
+import math
 
 
 def get_text():
@@ -43,25 +44,61 @@ def word_counter(input_text):
 def own_probability(input_text, number_of_words):
     #A method that takes the input map/list and calculates the own probability
     #Uses the formula: number of repetitions of a word divided by all words
-    all_values= list(input_text.values())
+    all_values = list(input_text.values())
     all_words = list(input_text.keys())
-    print(all_values)
-    print(all_words)
     own_probs = [x / number_of_words for x in all_values]
     print(own_probs)
     new_dict = dict(zip(all_words, own_probs))
-    new_dict = sorted(new_dict.items(), key=operator.itemgetter(1))
+    #Sorting dictionary makes it a list
+    return new_dict
+
+
+def cond_prob():
+    #TODO: Function that calculates the conditional probability of all words, based on the previous word
+    return 0
+
+
+def uniq_characters(input_text):
+    #Firstly we count how many different characters in the text
+    #we join the entire text into one word, then check for unique characters using len(set)
+    #TODO: Decide if uniq characters is lower case or lower and upper
+    one_string = "".join(input_text)
+    uniq_chars = len(set(one_string))
+    return uniq_chars
+
+def equal_entropy(input_text,uniq_chars,length):
+    #Function that calculates the entropy of every word, with the condition, THAT EACH WORDS PROBABILITIES ARE EQUAL(THE SAME)
+    #Equation used: sum(PROBABILITY *  log(Y)(PROBABILITY)
+    #Y = number of unique characters in the alphabet
+    probability = 1/length
+    logy_probability = math.fabs(math.log(probability, uniq_chars))
+    #No for loop needed, because every probability is equal
+    return logy_probability
+
+def own_entropy(input_text,uniq_chars):
+    #TODO: Function that calculates entropy using each word's own probability
+    #Uses the same formula equal entropy uses, but calculates a different probability
+    all_words = list(input_text.keys())
+    all_values = list(input_text.values())
+
+    for x in range(0, len(all_values)):
+        all_values[x] = (math.fabs(all_values[x] * math.log(all_values[x], uniq_chars)))
+    new_dict = dict(zip(all_words, all_values))
     return new_dict
 
 
 text = get_text()
 words = split_text(text)
 word_length = len(words)
+print("Words:")
 print(words)
 word_count = word_counter(words)
 print("Word counter:")
 print(word_count)
 own_probabilities = own_probability(word_count, word_length)
 print("Own probabilities(ascending):")
-print(own_probabilities)
-
+print(sorted(own_probabilities.items(), key=operator.itemgetter(1)))
+print("Equal entropy:")
+print(equal_entropy(word_count, uniq_characters(words), word_length))
+print("Own entropy:")
+print(own_entropy(own_probabilities, uniq_characters(words)))
